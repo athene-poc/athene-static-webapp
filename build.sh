43f72e32c2a77f2d9d1786b7cc4f3e0b0b3f76fc
Aljohn <47764438+aljohnnrma@users.noreply.github.com>
2020-04-16 22:37:35 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ echo "You just run the build.sh"
 pwd
 ls
 docker ps
-curl -u testuser:testpassword athene.com:5000/v2/_catalog
+#curl -u testuser:testpassword athene.com:5000/v2/_catalog
+curl -u testuser:testpassword 3.104.166.31:5000/v2/_catalog
